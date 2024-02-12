1d1023db942da134e35abe5ebcde2a0a8392d369
Kevin Do <kevin12345do@gmail.com>
2024-02-12 15:55:50 -0800

    GoalList class and tester files

---
+++ lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/GoalListTest.java
@@ -0,0 +1,20 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GoalListTest {
+
+    @Test
+    public void append() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void prepend() {
+    }
+}
---
+++ lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/GoalTest.java
@@ -0,0 +1,28 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GoalTest {
+
+    @Test
+    public void id() {
+    }
+
+    @Test
+    public void title() {
+    }
+
+    @Test
+    public void sortOrder() {
+    }
+
+    @Test
+    public void withId() {
+    }
+
+    @Test
+    public void withSortOrder() {
+    }
+}
