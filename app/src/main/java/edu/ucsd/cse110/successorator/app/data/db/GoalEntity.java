401e0de1f15fe6aea804eb65e7d9513b61d71155
Kevin Nie <mnie@ucsd.edu>
2024-02-16 23:47:12 -0800

    Goal Rollover, persistence needs to update for new field `lastUpdated`

--- app/src/main/java/edu/ucsd/cse110/successorator/app/data/db/GoalEntity.java
+++ app/src/main/java/edu/ucsd/cse110/successorator/app/data/db/GoalEntity.java
@@ -5,7 +5,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
-
+import java.util.Calendar;
 import edu.ucsd.cse110.successorator.lib.domain.Goal;
 import edu.ucsd.cse110.successorator.lib.domain.GoalRepository;
 
@@ -24,22 +24,30 @@ public class GoalEntity {
     @ColumnInfo(name = "sort_order")
     public int sortOrder;
 
+//    @ColumnInfo(name = "last_updated")
+//    public long lastUpdated;
+
     GoalEntity(@NonNull String title, int sortOrder) {
         this.title = title;
         this.sortOrder = sortOrder;
         this.isCompleted = false;
+//        this.lastUpdated = Calendar.getInstance().getTimeInMillis();
     }
 
     public static GoalEntity fromGoal(@NonNull Goal goal) {
         var card = new GoalEntity(goal.title(), goal.sortOrder());
         card.id = goal.id();
         card.isCompleted = goal.isCompleted();
+//        card.lastUpdated = goal.getLastUpdated().getTimeInMillis();
         return card;
     }
 
     public @NonNull Goal toGoal() {
         Goal goal = new Goal(id, title, sortOrder);
         goal.setIsCompleted(isCompleted);
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTimeInMillis(lastUpdated);
+//        goal.setLastUpdated(calendar);
         return goal;
     }
 }
